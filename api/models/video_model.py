b78779e979d70c5df8273f81e26f1aae77714529
Chris Giroux <cgiroux86@gmail.com>
2020-09-01 14:58:54 -0400

    pleasseee work

--- api/models/video_model.py
+++ api/models/video_model.py
@@ -1,17 +1,17 @@
-from api.models.user_model import db, User
+# from api.models.user_model import db, User
 
 
-class Video(db.Model):
-    __tablename__ = 'videos'
+# class Video(db.Model):
+#     __tablename__ = 'videos'
 
-    id = db.Column(db.Integer, primary_key=True)
-    # owner_id = db.Column(db.Integer(), db.ForeignKey(
-    #     'users.id'), nullable=False)
-    title = db.Column(db.String(), nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     # owner_id = db.Column(db.Integer(), db.ForeignKey(
+#     #     'users.id'), nullable=False)
+#     title = db.Column(db.String(), nullable=False)
 
-    def __init__(self, owner_id, title):
-        self.owner_id = owner_id
-        self.title = title
+#     def __init__(self, owner_id, title):
+#         self.owner_id = owner_id
+#         self.title = title
 
-    def __repr__(self):
-        return f'user_id: {self.owner_id}'
+#     def __repr__(self):
+#         return f'user_id: {self.owner_id}'
